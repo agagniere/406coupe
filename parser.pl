17a7ba45c25f2cef75730d5addff402722413cb4
Antoine GAGNIERE <sid.xxdzs@yahoo.com>
2020-08-09 00:05:02 +0200

    Parser now downloads images

--- parser.pl
+++ parser.pl
@@ -10,12 +10,6 @@ while (my $line = <STDIN>)
 {
 	chomp ($line);
 
-	#if ($line =~ /"body":"([:-_ a-zA-Z0-9]+)"/)
-	if ($line =~/<span class="_1fFkI">(.+?)<\/span>/)
-	{
-		$result{"Description"} = $1;
-	}
-
 	if (not $line =~ /"attributes":/) {
 		next;
 	}
@@ -26,29 +20,33 @@ while (my $line = <STDIN>)
 	if ($line =~ /"price":\[(\d+?)\]/) {
 		$result{"Price"} = int($1);
 	}
+	if ($line =~ /"body":"(.+?)"/) {
+		$result{"Description"} = $1;
+	}
+	if ($line =~ /"urls_large":\[(.+?)\]/) {
+		my $urls = $1;
+		while ($urls =~ /"(.+?\/(\w+\.[a-z]+))",?/g ) {
+			system "curl -O $1";
+			push @{$result{"Pictures"}}, $2
+		}
+	}
 
-	foreach my $key ( qw( brand issuance_date model vehicle_type ) ) {
+	foreach my $key ( qw(brand issuance_date model vehicle_type vehicle_color) ) {
 		if ($line =~ /"key":"$key","value":"(.+?)"/) {
 			$result{$key} = $1;
 		}
 	}
-	foreach my $key ( qw( mileage doors ) ) {
+	foreach my $key ( qw(mileage doors regdate seats horsepower horse_power_din) ) {
 		if ($line =~ /"key":"$key","value":"(\d+?)"/) {
 			$result{$key} = int($1);
 		}
 	}
-
-	if ($line =~ /"key":"fuel","value":"(\d+?)",.+?,"value_label":"([a-zA-Z]+)"/)
-	{
-		$result{"fuel_id"} = int($1);
-		$result{"Fuel"} = $2;
+	foreach my $key ( qw(fuel gearbox) ) {
+		if ($line =~ /"key":"$key","value":"(\d+?)",.+?,"value_label":"(\w+?)"/) {
+			$result{$key . "_id"} = int($1);
+			$result{$key} = $2;
+		}
 	}
-	if ($line =~ /"key":"gearbox","value":"(\d+?)",.+?,"value_label":"([a-zA-Z]+)"/)
-	{
-		$result{"gearbox_id"} = int($1);
-		$result{"Gearbox"} = $2;
-	}
-
 }
 
 print Dumper(\%result);
