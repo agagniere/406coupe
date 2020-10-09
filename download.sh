b4df66078d12cf9330e5381d48e0db7aca6b5206
Antoine GAGNIERE <sid.xxdzs@yahoo.com>
2020-10-09 12:44:30 +0200

    Include the link to the ad Title is not important

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Filename,Horsepower,Registration date,Gearbox,Fuel,Price,Mileage,Color" > data.csv
-echo "Title,Price,City,Brand,Model,Type,Registration date,Mileage,Horsepower,Fuel,Gearbox,Color" > ads.csv
+echo "Price,City,Brand,Model,Type,Registration date,Mileage,Horsepower,Fuel,Gearbox,Color,Link" > ads.csv
 for file in `find . -name '*.html'`
 do
 	echo $file
--- parser.pl
+++ parser.pl
@@ -26,15 +26,18 @@ while (my $line = <STDIN>)
 	if ($line =~ /"city_label":"(.+?)"/) {
 		$result{"City"} = $1;
 	}
+	if ($line =~ /"url":"https:\/\/(www.leboncoin.fr\/voitures\/\d+?.htm)"/) {
+		$result{"Link"} = $1;
+	}
 	if ($line =~ /"urls_large":\[(.+?)\]/) {
 		my $urls = $1;
 		while ($urls =~ /"(.+?\/(\w+\.[a-z]+))",?/g ) {
-			system "curl -O $1";
+#			system "curl -O $1";
 			push @{$result{"Pictures"}}, $2
 		}
 	}
 
-	foreach my $key ( qw(brand issuance_date model vehicle_type vehicle_color) ) {
+	foreach my $key ( qw(brand issuance_date model vehicle_type vehicule_color) ) {
 		if ($line =~ /"key":"$key","value":"(.+?)"/) {
 			$result{$key} = $1;
 		}
@@ -59,9 +62,9 @@ if (not defined $result{"issuance_date"} and defined $result{"regdate"}) {
 }
 
 foreach my $pic (@{$result{"Pictures"}}) {
-	print "$pic,$result{horse_power_din},$result{regdate},$result{gearbox_id},$result{fuel_id},$result{Price},$result{mileage},$result{vehicle_color}\n"
+	print "$pic,$result{horse_power_din},$result{regdate},$result{gearbox_id},$result{fuel_id},$result{Price},$result{mileage},$result{vehicule_color}\n"
 }
 
 open(FH, '>>', "ads.csv") or die $!;
-print FH "$result{Title},$result{Price},$result{City},$result{brand},$result{model},$result{vehicle_type},$result{issuance_date},$result{mileage},$result{horse_power_din},$result{fuel},$result{gearbox_id},$result{vehicle_color}";
+print FH "$result{Price},$result{City},$result{brand},$result{model},$result{vehicle_type},$result{issuance_date},$result{mileage},$result{horse_power_din},$result{fuel},$result{gearbox},$result{vehicule_color},$result{Link}\n";
 close(FH);
