dacfaa91409bd170eeac5c9917b926d91c3ac744
Antoine GAGNIERE <antoine.gagniere@yahoo.com>
2020-11-14 19:07:32 +0100

    Add year constraints to features

--- python/Feature.py
+++ python/Feature.py
@@ -1,56 +1,44 @@
 import csv
 
+START = 1997
+END = 2004
+
 class Feature:
 
-    def __init__(self, name, values):
+    def __init__(self, name, values, years = None):
         self.name = name
         self.values = values
+        self.years = years
         self.label_from_string = {s:i for i,s in enumerate(values)}
-        self.images_paths = []
-        self.labels = []
-        self.not_visible = []
-
-    def add_image(self, path, label):
-        self.images_paths += [path]
-        self.labels += [label]
-
-    def add_not_visible(self, path):
-        self.not_visible += [path]
-
-    def to_csv(self, file_name):
-        with open(file_name, mode='w') as f:
-            writer = csv.writer(f, delimiter=',')
-            writer.writerow(['Path', 'Label'])
-            writer.writerows(zip(self.images_paths, self.labels))
-
-    def from_csv(self, file_name):
-        with open(file_name, mode='r') as f:
-            self.from_opened_csv(f)
-
-    def from_opened_csv(self, csv_file):
-        reader = csv.reader(csv_file, delimiter=',')
-        next(reader)
-        for row in reader:
-            self.add_image(*row)
-
-    def boolean_training_balanced(self):
-        return (self.images_paths + self.not_visible[:len(self)], [True] * len(self) + [False] * min(len(self), len(self.not_visible)))
-    def boolean_training(self):
-        return (self.images_paths + self.not_visible, [True] * len(self) + [False] * len(self.not_visible))
-
-    def __len__(self):
-        return len(self.images_paths)
 
     def __str__(self):
-        return "Feature('{}', {} values, {} images)".format(self.name, len(self.values), len(self))
-
+        return "Feature('{}', {} values)".format(self.name, len(self.values))
 
 class Features:
-    rims = Feature("Rims", ["BBS", "Nautilus", "Hoggar", "Tacoma", "Other"])
-    front_bumper = Feature("Front Bumper", ["Phase 1", "Phase 2", "Other"])
-    interior = Feature('Interior', ["Abricot", "Amarante", "Ouragan", "Nil Bleu", "Alezan", "Salzbourg Astrakan", "Cobalt", "Ouragan Salzbourg", "Creme", "Chess"])
-    exterior = Feature("Exterior", ["Cendré", "Thallium", "Hadès", "Cosmos", "Riviera", "Hypérion", "Bysance", "Récife", "Ecarlate", "Lucifer", "Lugano", "Polo", "Solstice", "Louxor", "Granit", "Other"])
-    engine = Feature("Engine", ["XU10J4R 135", "EW10J4 137", "HDi", "2.2 160", "V6 194", "V6 210"])
+    rims = Feature(
+        "Rims",
+        ["BBS",         "Nautilus",  "Hoggar",    "Tacoma"],
+        [(START, 2001), (2002, END), (2001, END), (2003,END)])
+
+    front_bumper = Feature(
+        "Front Bumper",
+        ["Phase 1",     "Phase 2"],
+        [(START, 2003), (2003, END)])
+
+    interior = Feature(
+        "Interior",
+        ["Abricot",     "Amarante",    "Ouragan",    "Nil Bleu",    "Alezan",    "Salzbourg Astrakan", "Cobalt",    "Ouragan Salzbourg", "Creme",      "Chess"],
+        [(START, 2000), (START, 2003), (START, END), (START, 2000), (2000, END), (2000, 2003),         (2003, END), (2003, END),         (2000, 2000), (2002, 2002)])
+
+    exterior = Feature(
+        "Exterior",
+        ["Cendré", "Thallium", "Hadès", "Cosmos", "Riviera", "Hypérion", "Bysance", "Récife", "Ecarlate", "Lucifer", "Lugano", "Polo", "Solstice", "Louxor", "Granit"])
+
+    engine = Feature(
+        "Engine",
+        ["XU10J4R 135", "EW10J4 137", "HDi",       "2.2 160",   "V6 194",      "V6 210"],
+        [(START, 1999), (1999, 2002), (2001, END), (2002, END), (START, 1999), (1999, END)])
+
     dict = {'rims':rims, 'front_bumper':front_bumper, 'interior':interior, 'exterior':exterior, 'engine':engine}
     list = list(dict.keys())
 
